dcb90bc6499f5be2d77e9fb55c6043c7474570c4
matteovecchio04 <matteovex@gmail.com>
2025-11-13 12:52:33 +0100

    add initial data form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import axios from "axios"
 
 export default function App() {
 
+  const endpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
+
+  const dataForm = {
+    author: ""
+    title: ""
+    body: ""
+    public: ""
+  }
+
   return (
     <>
 
